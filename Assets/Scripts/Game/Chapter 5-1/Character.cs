4f5debf34d611080c42e79b5ec713bee1feec3ed
yucheon6000 <yucheon6000@gmail.com>
2022-07-22 17:57:27 +0900

    앨리스 애니메이션 버그 수정 및 점프/랜드 딜레이 추가

--- Assets/Scripts/Game/Chapter 5-1/Character.cs
+++ Assets/Scripts/Game/Chapter 5-1/Character.cs
@@ -8,18 +8,30 @@ namespace Chapter_5_1
     public class Character : MonoBehaviour, IJumpable, ITurnable, ICompletable
     {
         protected Vector3 spawnPosition;                    // 스폰 위치 (처음 위치)
+        [Header("Move")]
         [SerializeField]
         protected float moveSpeed = 1f;                     // 이동 속도
         protected float moveSpeedDuringJump = 0;            // 점프하는 동안의 이동 속도
         protected Vector3 moveDirection = Vector3.right;    // 이동 방향
+
+        [Header("Jump")]
         [SerializeField]
         protected float jumpForce = 5f;                     // 점프 힘
         protected bool isJumping = false;                   // 점프 중인지 여부
-        protected bool isGround = false;                    // 바닥에 닿았는지 여부
+        [SerializeField]
+        protected float jumpDelayTime = 0.25f;              // 점프 앞에 쉬는 시간
+        [SerializeField]
+        private float landDelayTime = 0.75f;                // 착지 후 쉬는 시간
 
-        protected new Rigidbody2D rigidbody2D;
+        [Header("Components")]
         [SerializeField]
         protected CharacterAnimator characterAnimator;
+        protected new Rigidbody2D rigidbody2D;
+
+        [Header("@Debug")]
+        [SerializeField]
+        protected bool isGround = false;                    // 바닥에 닿았는지 여부
+
 
         protected virtual void Awake()
         {
@@ -53,6 +65,11 @@ namespace Chapter_5_1
             // 땅에 닿지 않았거나, 점프 중이면 리턴
             if (!isGround || isJumping) return;
 
+            StartCoroutine(JumpRoutine(jumpForce, moveSpeedDuringJump));
+        }
+
+        private IEnumerator JumpRoutine(float jumpForce, float moveSpeedDuringJump)
+        {
             // 상태 업데이트
             isGround = false;
             isJumping = true;
@@ -60,10 +77,32 @@ namespace Chapter_5_1
             // 점프 중 이동속도 설정
             this.moveSpeedDuringJump = moveSpeedDuringJump;
 
+            // 애니메이션 재생
+            characterAnimator?.Jump();
+
+            // 점프 딜레이가 있으면 멈춰서 쉬고 다시 진행
+            if (jumpDelayTime > 0)
+            {
+                Vector3 temp = moveDirection;
+
+                Turn(Vector3.zero);
+                yield return new WaitForSeconds(jumpDelayTime);
+                Turn(temp);
+            }
+
             // 점프
             rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
 
-            characterAnimator?.Jump();
+        private IEnumerator LandRoutine()
+        {
+            if (landDelayTime <= 0) yield break;
+
+            Vector3 temp = moveDirection;
+
+            Turn(Vector3.zero);
+            yield return new WaitForSeconds(landDelayTime);
+            Turn(temp);
         }
         #endregion
 
@@ -83,10 +122,14 @@ namespace Chapter_5_1
         #region Collision
         protected void OnCollisionEnter2D(Collision2D other)
         {
+            if (isGround) return;
+
             if (other.gameObject.CompareTag("Ground"))
             {
                 isGround = true;
                 isJumping = false;
+
+                StartCoroutine(nameof(LandRoutine));
 
                 characterAnimator?.Land();
             }
